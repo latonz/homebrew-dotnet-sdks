bccac2ad49cf628fa59486a47044a2ec8d7f7797
riokritchie <47655883+riokritchie@users.noreply.github.com>
2022-12-14 09:11:26 +0100

    Update formulas (created by auto updater) (#11)
    Co-authored-by: latonz <latonz@users.noreply.github.com>

--- Casks/dotnet-sdk-6-0-404.rb
+++ Casks/dotnet-sdk-6-0-404.rb
@@ -1,4 +1,4 @@
-cask "dotnet-sdk-6-0" do
+cask "dotnet-sdk-6-0-404" do
     version "6.0.404,6.0.12"
     
     if Hardware::CPU.intel?
--- Casks/dotnet-sdk-7-0-101.rb
+++ Casks/dotnet-sdk-7-0-101.rb
@@ -1,4 +1,4 @@
-cask "dotnet-sdk-7-0" do
+cask "dotnet-sdk-7-0-101" do
     version "7.0.101,7.0.1"
     
     if Hardware::CPU.intel?
